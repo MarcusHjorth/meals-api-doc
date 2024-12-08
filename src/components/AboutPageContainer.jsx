3dc07f3a2ff7bf26dfd40bbbd4bdbbefd7d1ee8e
MateenCPH <cph-mr645@cphbusiness.dk>
2024-12-08 15:36:12 +0100

    minor

--- src/components/AboutPageContainer.jsx
+++ src/components/AboutPageContainer.jsx
@@ -4,12 +4,18 @@ export const PageContainer = styled.div`
   padding: 20px;
   background-color: #fefefe;
   color: #333;
-  font-family: 'Arial', sans-serif;
+  font-family: "Arial", sans-serif;
 `;
 
 export const Title = styled.h1`
   font-size: 2.5rem;
   color: #ff69b4;
+  text-align: center;
+`;
+
+export const Middletitle = styled.h2`
+  color: #555;
+  margin-bottom: 1rem;
   text-align: center;
 `;
 
